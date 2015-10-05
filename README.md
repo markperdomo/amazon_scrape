19adde3296a8056f2e4b54ad6e133b639a1cf9da
Mark Perdomo <markperdomo@gmail.com>
2015-10-04 20:34:45 -0400

    Random sampling functionality built

---
+++ README.md
@@ -0,0 +1,10 @@
+Amazon Scrape
+=============
+
+To support a project that relies on Amazon data, we want to randomly sample books listed on Amazon, grab relevant information, and dump to excel, in order to support a statistical study.
+
+Step 1: Grab book links from http://www.bookbookgoose.com/
+
+Step 2: Access Amazon and grab data
+
+Step 3: Write to Excel
--- amazon_scrape.py
+++ amazon_scrape.py
@@ -1,2 +1,39 @@
 #!env/bin/python
 
+"""
+AmazonScrape
+
+Usage: amazon_scrape.py [--number=<count>] [--output=output.xlsx]
+
+Select and scrape a given number of Amazon book listings.
+"""
+
+from docopt import docopt
+import requests
+from bs4 import BeautifulSoup
+import json
+
+        
+def random_amazon_link(number):
+    """Use the site http://www.bookbookgoose.com/ to generate a random
+    amazon link.  Grab the page and slice it apart for a link"""
+    r = requests.get('http://www.bookbookgoose.com/v1/get_books?n={0}'.format(number))
+    response = r.json()
+    books = []
+    for item in response:
+        book = {
+        'author':item[0],
+        'title':item[1],
+        'link':item[2],
+        }
+        books.append(book)
+    return books
+        
+
+def main():
+    arguments = docopt(__doc__)
+    print random_amazon_link(5)
+
+
+if __name__ == '__main__':
+    main()
